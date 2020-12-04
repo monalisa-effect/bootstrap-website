64773cc91dd48a4851f09ffbac9766ee5073bb2e
Monalisa Egobishi <55362351+monalisa-effect@users.noreply.github.com>
2020-12-04 20:54:26 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    // Cache the window object
+
+    var $window = $(window);
+
+    //parallax backgroung effect
+    
+    $('section[data-type="background"]').each(function(){
+
+        var $bgobj = $(this);
+
+        $(window).scroll(function () {
+            var yPos= -($window.scrollTop()/ $bgobj.data('speed'));
+
+            var coords = '50% ' + yPos + 'px' ;
+
+            $bgobj.css({backgroundPosition:coords});
+        });
+    });
+});
